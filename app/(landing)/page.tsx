e4686a0ca5a5cac7191ca9f411c94093ed9d9a2e
zcy22606 <619730086@qq.com>
2024-03-21 00:37:00 +0800

    feat: optimizing images

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main
       className="
     flex-1 bg-background w-screen relative flex justify-center backdrop-brightness-150 items-center
-    bg-[url('/home_light.png')] bg-bottom bg-cover dark:bg-[url('/home.jpg')]
+    bg-[url('/home_light.jpg')] bg-bottom bg-cover dark:bg-[url('/home.jpg')]
     "
     >
       <h1 className="text-[56px] font-bold relative z-10">测试网址</h1>
